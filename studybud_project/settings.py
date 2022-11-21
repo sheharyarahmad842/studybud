ddb715c56023ec05fb922ef4ffa2981cb062e052
Sheharyar Ahmad <phantomwitch35@gmail.com>
2022-11-21 23:15:40 +0500

    Update allowed_hosts setting

--- studybud_project/settings.py
+++ studybud_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["phantom-social-app.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
