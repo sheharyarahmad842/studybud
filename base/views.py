dd4a9895e041cda017e07dc201be9c45c3b2f1c6
Sheharyar Ahmad <phantomwitch35@gmail.com>
2022-11-22 19:27:49 +0500

    Change the template of update view

--- base/views.py
+++ base/views.py
@@ -78,7 +78,7 @@ class RoomCreateView(LoginRequiredMixin, CreateView):
 class RoomUpdateView(LoginRequiredMixin, UpdateView):
     model = Room
     fields = ("topic", "name", "description")
-    template_name = "base/room_update.html"
+    template_name = "base/room_form.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
