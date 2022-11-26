a283f9fc649365f72681e9d508598e64c0ec6d24
Sheharyar Ahmad <phantomwitch35@gmail.com>
2022-11-26 11:53:03 +0500

    Modify settings file for production

--- studybud_project/settings.py
+++ studybud_project/settings.py
@@ -232,7 +232,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_USER = "phantomwitch35@gmail.com"
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 # Django debug toolbar configuration for docker
