9e3629ecaf6a698167365fb4bca6cb22aea02117
Sheharyar Ahmad <sheharyarahmad842@gmail.com>
2023-09-30 16:33:54 +0500

    modify script file for production

--- build.sh
+++ build.sh
@@ -6,7 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then
-    python manage.py createsu
-fi
+python manage.py createsu
